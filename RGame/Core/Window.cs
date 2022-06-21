b2c260749aa1fcc8f5bfcc039db98a938659a73d
Mi1ax <yuriymilax@icloud.com>
2022-06-21 20:51:24 +0300

    Remove unncessary stuff

--- RGame/Core/Window.cs
+++ RGame/Core/Window.cs
@@ -7,79 +7,38 @@ namespace RGame.Core
 {
     public class Window
     {
-        private Vector2 m_ViewportSize;
-        private readonly RenderTarget m_Target;
         private bool m_Running = true;
 
         public Action<float>? OnUpdate = null;
         public Action? OnDraw = null;
-        public Action? OnTargetDraw = null;
         public Action? OnImGuiDraw = null;
 
-        public Window()
+        public Window(string label, int width, int height)
         {
-            SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_VSYNC_HINT);
-            InitWindow(1280, 800, "Window");
-            SetTargetFPS(144);
+            InitWindow(width, height, label);
 
             rlImGui.Setup();
-            m_Target = new RenderTarget();
-            m_ViewportSize = Vector2.Zero;
-        }
-
-        private void Update()
-        {
-            if (m_ViewportSize != new Vector2(m_Target.GetWidth(), m_Target.GetHeight()))
-                m_Target.Resize(m_ViewportSize);
-
-            OnUpdate?.Invoke(GetFrameTime());
-        }
-
-        private void DrawToTexture(RenderTarget target)
-        {
-            BeginTextureMode((RenderTexture2D)target);
-            ClearBackground(Color.DARKGRAY);
-
-            OnTargetDraw?.Invoke();
-
-            EndTextureMode();
-        }
-
-        private void Draw()
-        {
-            rlImGui.Begin();
-            Docking.Draw(() => {
-                m_ViewportSize = ImGui.GetContentRegionAvail();
-                ImGui.Image(
-                    m_Target.GetTextureIDPtr(),
-                    m_ViewportSize,
-                    new Vector2(0, 1),
-                    new Vector2(1, 0)
-                    );
-            }, OnImGuiDraw);
-            rlImGui.End();
-
-            OnDraw?.Invoke();
         }
 
         public void Run() 
         {
             while (!WindowShouldClose() && m_Running)
             {
-                Update();
-
-                DrawToTexture(m_Target);
+                OnUpdate?.Invoke(GetFrameTime());
 
                 BeginDrawing();
                 ClearBackground(Color.PINK);
                 {
-                    Draw();
+                    OnDraw?.Invoke();
+
+                    rlImGui.Begin();
+                    OnImGuiDraw?.Invoke();
+                    rlImGui.End();
                 }
                 EndDrawing();
             }
 
             rlImGui.Shutdown();
-            m_Target.Free();
             CloseWindow();
         }
 
