cbcc1f0824afd69bf9a0e79e1b5140a4e46400f8
Mi1ax <yuriymilax@icloud.com>
2022-06-22 14:26:16 +0300

    Camera movement (Not finished)

---
+++ RGame/Core/RColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace RGame.Core;
+
+public class RColor
+{
+    private Vector4 m_Color;
+
+    public RColor(float r, float g, float b, float a)
+    {
+        m_Color = new Vector4(r, g, b, a);
+    }
+
+    public ref Vector4 GetColorRef() => ref m_Color;
+
+    public static implicit operator RColor(Color color) => new (
+        color.r / 255.0f, 
+        color.g / 255.0f, 
+        color.b / 255.0f, 
+        color.a / 255.0f);
+
+    public static explicit operator Color(RColor color) => new (
+        (int)(color.m_Color.X * 255.0f),
+        (int)(color.m_Color.Y * 255.0f),
+        (int)(color.m_Color.Z * 255.0f),
+        (int)(color.m_Color.W * 255.0f));
+}
--- RGame/Core/Window.cs
+++ RGame/Core/Window.cs
@@ -1,7 +1,10 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+
+using Raylib_cs;
 using ImGuiNET;
+
 using static Raylib_cs.Raylib;
-using System.Numerics;
+using static Raylib_cs.Rlgl;
 
 namespace RGame.Core
 {
@@ -13,11 +16,20 @@ namespace RGame.Core
         public Action? OnDraw = null;
         public Action? OnImGuiDraw = null;
 
+        private readonly Vector3 m_CubePosition;
+
+        private readonly RCamera3D m_Camera;
+        private readonly RColor m_Color;
+        
         public Window(string label, int width, int height)
         {
+            SetConfigFlags(ConfigFlags.FLAG_VSYNC_HINT | ConfigFlags.FLAG_MSAA_4X_HINT);
             InitWindow(width, height, label);
-
             rlImGui.Setup();
+
+            m_CubePosition = Vector3.Zero;
+            m_Camera = new RCamera3D();
+            m_Color = Color.RED;
         }
 
         public void Run() 
@@ -25,14 +37,44 @@ namespace RGame.Core
             while (!WindowShouldClose() && m_Running)
             {
                 OnUpdate?.Invoke(GetFrameTime());
+                m_Camera.Update(GetFrameTime());
 
                 BeginDrawing();
-                ClearBackground(Color.PINK);
                 {
-                    OnDraw?.Invoke();
+                    ClearBackground(Color.GRAY);
+
+                    //BeginMode3D(m_Camera.GetCameraHandler());
+                    m_Camera.BeginDraw();
+                    {
+                        DrawCube(m_CubePosition, 2.0f, 2.0f, 2.0f, (Color)m_Color);
+                        DrawCubeWires(m_CubePosition, 2.0f, 2.0f, 2.0f, Color.MAROON);
+
+                        DrawGrid(40, 1.0f);
+                    }
+                    m_Camera.EndDraw();
+
+                    DrawFPS(10, 10);
 
                     rlImGui.Begin();
-                    OnImGuiDraw?.Invoke();
+                    ImGui.Begin("Cube");
+                    {
+                        ImGui.ColorEdit4("Color", ref m_Color.GetColorRef());
+                    }
+                    ImGui.End();
+
+                    ImGui.Begin("Camera");
+                    {
+                        ImGui.DragFloat2("Angle", ref m_Camera.Angle, 0.01f);
+                        ImGui.DragFloat("Target Distance", ref m_Camera.TargetDistance);
+
+                        ImGui.Separator();
+
+                        ImGui.DragFloat3("Position", ref m_Camera.Position);
+                        ImGui.DragFloat3("Target", ref m_Camera.Target);
+                        ImGui.DragFloat3("Up", ref m_Camera.Up);
+                        ImGui.DragFloat("Fov", ref m_Camera.Fovy);
+                    }
+                    ImGui.End();
                     rlImGui.End();
                 }
                 EndDrawing();
